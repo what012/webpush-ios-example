79f954ae7eb67f705354f898b113d369135fb63b
what012 <107616108+what012@users.noreply.github.com>
2026-02-09 10:30:00 +0100

    Idk

--- sw.js
+++ sw.js
@@ -2,33 +2,22 @@ self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
+self.addEventListener('activate', e) => {
   e.waitUntil(clients.claim());
 });
 
 self.addEventListener('message', (e) => {
   if (e.data && e.data.type === 'SHOW_NOTIFICATION') {
-    const { title, body, tag } = e.data;
+    const { title, body, tag, image } = e.data;
+    const opts = {
+      body: body || '',
+      tag: tag || undefined,
+      silent: true,
+      renotify: !!tag,
+    };
+    if (image) opts.image = image;
     e.waitUntil(
-      self.registration.showNotification(title || 'Notification', {
-        body: body || '',
-        tag: tag || undefined,
-        silent: true,
-        renotify: !!tag,
-      })
-    );
-  }
-
-  // support receiving a batch of notifications in one message
-  if (e.data && e.data.type === 'SHOW_NOTIFICATION_BATCH' && Array.isArray(e.data.notifications)) {
-    const notifs = e.data.notifications;
-    e.waitUntil(
-      Promise.all(notifs.map(n => {
-        const t = n.title || 'Notification';
-        const b = n.body || '';
-        const tg = n.tag || undefined;
-        return self.registration.showNotification(t, { body: b, tag: tg, silent: true, renotify: !!tg });
-      }))
+      self.registration.showNotification(title || 'Notification', opts)
     );
   }
 });
@@ -40,7 +29,7 @@ self.addEventListener('notificationclick', (e) => {
       if (cls.length > 0) {
         cls[0].focus();
       } else {
-        clients.openWindow('./');
+        clients.openWindow('/');
       }
     })
   );
